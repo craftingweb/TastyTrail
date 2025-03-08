85ea3efdf7a7d3187f9d4816c76bbf9147080b94
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2025-03-08 11:29:00 -0500

    adding styling for meals

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,7 +1,8 @@
 export default function Meals() {
   return (
-    <main>
-      <h1 style={{ color: "blue", textAlign: "center" }}>Time to get meal!</h1>
-    </main>
+    <>
+      <header></header>
+      <main></main>
+    </>
   );
 }
