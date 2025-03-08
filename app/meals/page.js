31e7c8e21f791f6293885dd62729988f513bade1
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2025-03-08 11:55:27 -0500

    adding meals itme

--- app/meals/page.js
+++ app/meals/page.js
@@ -1,8 +1,22 @@
+import Link from "next/link";
+import classes from "./page.module.css";
+
 export default function Meals() {
   return (
     <>
-      <header></header>
-      <main></main>
+      <header className={classes.header}>
+        <h1>
+          Delicious meals, created
+          <span className={classes.highlight}>by you</span>
+        </h1>
+        <p>
+          Choose you favorite recipe and cook it yourself. It is easy and fun!
+        </p>
+        <p className={classes.cta}>
+          <Link href="/meals/share"> Share your favorite recipe </Link>
+        </p>
+      </header>
+      <main className={classes.main}></main>
     </>
   );
 }
