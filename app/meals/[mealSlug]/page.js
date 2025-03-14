5c23c4e24e946f46d1a7e1aa989a0ad0c60f0984
From 0 to 1 <101225909+VitaliPri@users.noreply.github.com>
2025-03-14 11:14:42 -0400

    adding styles for Meal Details

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,8 @@
+export default function MealDetailsPage() {
+  return (
+    <>
+      <header></header>
+      <main></main>
+    </>
+  );
+}
--- app/meals/[url]/page.js
+++
@@ -1,9 +0,0 @@
-export default function dynamic() {
-  return (
-    <main>
-      <h1 style={{ color: "green", textAlign: "center" }}>
-        Time to get any content!
-      </h1>
-    </main>
-  );
-}
